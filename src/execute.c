be08f91b38c424656784ab0f8084b222e9c14eb4
Mikhail Melekhin <melekhinmk@gmail.com>
2026-01-04 03:08:57 +0500

    Redirect output from stdout/stderr to the specified file using dup().

--- src/execute.c
+++ src/execute.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/wait.h>
 #include "shell.h"
 
@@ -15,19 +16,32 @@ const char *builtin_names[] = {
 };
 
 int execute_binary(char **args);
+int get_original_fd(redirection_state state);
+int set_redirection(char *filename, redirection_state state);
 
 int shell_execute(environment_var *env) {
     if (env->args == NULL) {
         return 1;
     }
 
+    int original_fd = get_original_fd(env->redirection);
+    int saved_state = set_redirection(env->ofile, env->redirection);
+
+    int result = -1;
     for (int i = 0; builtin_names[i] != NULL; i++) {
         if (!strcmp(env->args[0], builtin_names[i])) {
-            return execute_builtin(env, i);
+            result = execute_builtin(env, i);
         }
     }
+    if (result == -1)
+        result = execute_binary(env->args);
 
-    return execute_binary(env->args);
+    if (saved_state != -1) {
+        dup2(saved_state, original_fd);
+        close(saved_state);
+    }
+
+    return result;
 }
 
 int execute_binary(char **args) {
@@ -51,4 +65,55 @@ int execute_binary(char **args) {
     }
 
     return 1;
+}
+
+int get_original_fd(redirection_state state) {
+    switch (state)
+    {
+    case NOT_REDIRECTING:
+        return -1;
+    case WRITING_STDOUT: case APPENDING_STDOUT:
+        return STDOUT_FILENO;
+    case WRITING_STDERR: case APPENDING_STDERR:
+        return STDERR_FILENO;
+    default:
+        return -1;
+    }
+}
+
+int set_redirection(char *filename, redirection_state state) {
+    int saved_state = -1;
+    int file = -1;
+
+    switch (state)
+    {
+    case NOT_REDIRECTING:
+        break;
+    case WRITING_STDOUT:
+        saved_state = dup(STDOUT_FILENO);
+        file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        dup2(file, STDOUT_FILENO);
+        break;
+    case APPENDING_STDOUT:
+        saved_state = dup(STDOUT_FILENO);
+        file = open(filename, O_WRONLY | O_APPEND | O_CREAT, 0644);
+        dup2(file, STDOUT_FILENO);
+        break;
+    case WRITING_STDERR:
+        saved_state = dup(STDERR_FILENO);
+        file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        dup2(file, STDERR_FILENO);
+        break;
+    case APPENDING_STDERR:
+        saved_state = dup(STDERR_FILENO);
+        file = open(filename, O_WRONLY | O_APPEND | O_CREAT, 0644);
+        dup2(file, STDERR_FILENO);
+        break;
+    default:
+        break;
+    }
+
+    if (file != -1)
+        close(file);
+    return saved_state;
 }
