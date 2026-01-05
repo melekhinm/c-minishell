f7199082e520ff605f38a4772df22661b5e8a743
Mikhail Melekhin <melekhinmk@gmail.com>
2026-01-05 17:30:45 +0500

    Add support for autocompleting PATH executables.

--- src/completion.c
+++ src/completion.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <dirent.h>
 #include "shell.h"
 
 char *command_generator(const char *text, int state);
@@ -14,17 +15,55 @@ char **my_completion(const char *text, int start, int end) {
 
 char *command_generator(const char *text, int state) {
     static int index, len;
+    static DIR *current_dir;
+    static char *path_env;
+    static char *token;
     char *name;
+    struct dirent *file;
 
     if (state == 0) {
         index = 0;
         len = strlen(text);
+        if (path_env)
+            free(path_env);
+        path_env = strdup(getenv("PATH"));
+        token = strtok(path_env, ":");
+        if (current_dir) {
+            closedir(current_dir);
+            current_dir = NULL;
+        }
     }
 
-    while ((name = builtin_names[index++])) {
-        if (!strncmp(name, text, len))
-            return strdup(name);
+    if (index != -1) {
+        while ((name = builtin_names[index++])) {
+            if (!strncmp(name, text, len)) {
+                return strdup(name);
+            }
+        }
+        index = -1;
     }
 
+    while (token || current_dir) {
+        if (current_dir == NULL) {
+            if (token == NULL)
+                break;
+            current_dir = opendir(token);
+            token = strtok(NULL, ":");
+            if (current_dir == NULL)
+                continue; 
+        }
+        while ((file = readdir(current_dir))) {
+            name = file->d_name;
+
+            if (!strncmp(name, text, len)) {
+                return strdup(name);
+            }
+        }
+        closedir(current_dir);
+        current_dir = NULL;
+    }
+
+    free(path_env);
+    path_env = NULL;
     return NULL;
 }
